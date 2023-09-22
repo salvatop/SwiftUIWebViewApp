f4311114519233dea7abe12e0fe9d78cb18e26b5
Salvatore Palazzo <salvatop78@gmail.com>
2023-09-22 13:22:25 -0400

    Initial Commit

---
+++ SwiftUIWebViewApp/SwiftUIWebViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWebViewAppApp.swift
+//  SwiftUIWebViewApp
+//
+//  Created by little-ac on 2023-09-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWebViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
